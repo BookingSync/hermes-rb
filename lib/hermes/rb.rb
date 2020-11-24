1a9cd8d62ce901b586e274ab5cf132a8cf0e6398
Karol Galanciak <karol.galanciak@gmail.com>
2020-11-24 06:45:57 +0100

    initial commit

---
+++ lib/hermes/rb.rb
@@ -0,0 +1,8 @@
+require "hermes/rb/version"
+
+module Hermes
+  module Rb
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/hermes/rb_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Hermes::Rb do
+  it "has a version number" do
+    expect(Hermes::Rb::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
