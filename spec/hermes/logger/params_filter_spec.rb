15a243a6cbc5f9e236560cef8ee83494448d2f61
Karol Galanciak <karol.galanciak@gmail.com>
2021-02-19 11:15:20 +0100

    fix params filter for nil values

--- spec/hermes/logger/params_filter_spec.rb
+++ spec/hermes/logger/params_filter_spec.rb
@@ -5,76 +5,94 @@ RSpec.describe Hermes::Logger::ParamsFilter do
 
       let(:value) { "value" }
 
-      describe "for attributes containing 'token' word" do
-        let(:attribute) { :access_token }
+      context "when value is nil" do
+        let(:value) { nil }
 
-        it { is_expected_block.to change { value }.from("value").to("[STRIPPED]") }
+        describe "for attributes containing sensitive keyword" do
+          let(:attribute) { :access_token }
+
+          it { is_expected_block.not_to change { value } }
+        end
+
+        describe "for attributes not containing sensitive keyword" do
+          let(:attribute) { :other }
+
+          it { is_expected_block.not_to change { value } }
+        end
       end
 
-      describe "for attributes containing 'password' word" do
-        let(:attribute) { :password_confirmation }
+      context "when value is not nil" do
+        describe "for attributes containing 'token' word" do
+          let(:attribute) { :access_token }
 
-        it { is_expected_block.to change { value }.from("value").to("[STRIPPED]") }
-      end
+          it { is_expected_block.to change { value }.from("value").to("[STRIPPED]") }
+        end
 
-      describe "for attributes containing 'credit_card' word" do
-        let(:attribute) { :credit_card }
+        describe "for attributes containing 'password' word" do
+          let(:attribute) { :password_confirmation }
 
-        it { is_expected_block.to change { value }.from("value").to("[STRIPPED]") }
-      end
+          it { is_expected_block.to change { value }.from("value").to("[STRIPPED]") }
+        end
 
-      describe "for attributes containing 'card_number' word" do
-        let(:attribute) { :card_number }
+        describe "for attributes containing 'credit_card' word" do
+          let(:attribute) { :credit_card }
 
-        it { is_expected_block.to change { value }.from("value").to("[STRIPPED]") }
-      end
+          it { is_expected_block.to change { value }.from("value").to("[STRIPPED]") }
+        end
 
-      describe "for attributes containing 'card_number' word" do
-        let(:attribute) { :card_number }
+        describe "for attributes containing 'card_number' word" do
+          let(:attribute) { :card_number }
 
-        it { is_expected_block.to change { value }.from("value").to("[STRIPPED]") }
-      end
+          it { is_expected_block.to change { value }.from("value").to("[STRIPPED]") }
+        end
 
-      describe "for attributes containing 'verification_value' word" do
-        let(:attribute) { :verification_value }
+        describe "for attributes containing 'card_number' word" do
+          let(:attribute) { :card_number }
 
-        it { is_expected_block.to change { value }.from("value").to("[STRIPPED]") }
-      end
+          it { is_expected_block.to change { value }.from("value").to("[STRIPPED]") }
+        end
 
-      describe "for attributes containing 'private_key' word" do
-        let(:attribute) { :private_key }
+        describe "for attributes containing 'verification_value' word" do
+          let(:attribute) { :verification_value }
 
-        it { is_expected_block.to change { value }.from("value").to("[STRIPPED]") }
-      end
+          it { is_expected_block.to change { value }.from("value").to("[STRIPPED]") }
+        end
 
-      describe "for attributes containing 'signature' word" do
-        let(:attribute) { :signature }
+        describe "for attributes containing 'private_key' word" do
+          let(:attribute) { :private_key }
 
-        it { is_expected_block.to change { value }.from("value").to("[STRIPPED]") }
-      end
+          it { is_expected_block.to change { value }.from("value").to("[STRIPPED]") }
+        end
 
-      describe "for attributes containing 'api_key' word" do
-        let(:attribute) { :api_key }
+        describe "for attributes containing 'signature' word" do
+          let(:attribute) { :signature }
 
-        it { is_expected_block.to change { value }.from("value").to("[STRIPPED]") }
-      end
+          it { is_expected_block.to change { value }.from("value").to("[STRIPPED]") }
+        end
 
-      describe "for attributes containing 'secret_key' word" do
-        let(:attribute) { :secret_key }
+        describe "for attributes containing 'api_key' word" do
+          let(:attribute) { :api_key }
 
-        it { is_expected_block.to change { value }.from("value").to("[STRIPPED]") }
-      end
+          it { is_expected_block.to change { value }.from("value").to("[STRIPPED]") }
+        end
 
-      describe "for attributes containing 'publishable_key' word" do
-        let(:attribute) { :publishable_key }
+        describe "for attributes containing 'secret_key' word" do
+          let(:attribute) { :secret_key }
 
-        it { is_expected_block.to change { value }.from("value").to("[STRIPPED]") }
-      end
+          it { is_expected_block.to change { value }.from("value").to("[STRIPPED]") }
+        end
 
-      describe "for other attributes" do
-        let(:attribute) { :currency }
+        describe "for attributes containing 'publishable_key' word" do
+          let(:attribute) { :publishable_key }
 
-        it { is_expected_block.not_to change { value } }
+          it { is_expected_block.to change { value }.from("value").to("[STRIPPED]") }
+        end
+
+        describe "for other attributes" do
+          let(:attribute) { :currency }
+
+          it { is_expected_block.not_to change { value } }
+        end
       end
     end
 
